69404340bfad46b080f73bbb8d48d78aa823a2e5
Ivaylo Plashkov <n3k0ne@gmail.com>
2018-03-05 12:49:00 +0200

    Add files via upload

---
+++ Objects, Inheritance and Prototypes - Exercise/04.Extensible object.js
@@ -0,0 +1,16 @@
+function getExtensibleObj(template) {
+    let obj = {
+        extend: (template) => {
+            for (let property in template) {
+                if(typeof template[property] === 'function'){
+                    Object.getPrototypeOf(obj)[property] = template[property];
+                }else{
+                    obj[property] = template[property];
+                }
+            }
+        }
+    };
+    return obj;
+}
+let extensibleObj = getExtensibleObj();
+extensibleObj.extend()
